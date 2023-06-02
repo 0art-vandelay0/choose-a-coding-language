c9725d4622a68b6fc34d223df67c3bdce118e3b9
Casey Hill <97810619+0art-vandelay0@users.noreply.github.com>
2023-06-02 09:07:53 -0700

    Add functioning branching statements

--- js/scripts.js
+++ js/scripts.js
@@ -12,15 +12,39 @@ window.onload = function () {
         const q1 = document.querySelector("input#q1").checked;
         const q2 = document.querySelector("input#q2").checked;
         const q3 = document.querySelector("input#q3").checked;
-
-        if (q1 && q2 && q3) {
-            document.getElementById("python").removeAttribute("class");
-        } else if ((q1 && q2) || (q2 && q3) || (q1 && q3)) {
-            document.getElementById("javaScript").removeAttribute("class");
-        } else if (q1 || q2 || q3) {
-            document.getElementById("rust").removeAttribute("class");
-        } else {
+        const q4 = document.querySelector("input#q4").checked;
+        const q5 = document.querySelector("input#q5").checked;
+        if (!q1 && !q2 && !q3 && !q4 && !q5) {
             document.getElementById("errorMessage").removeAttribute("class");
+        } else {
+            if (q1) {
+                document.getElementById("python").removeAttribute("class");
+            }
+            if (q2) {
+                document.getElementById("javaScript").removeAttribute("class");
+            }
+            if (q3) {
+                document.getElementById("rust").removeAttribute("class");
+            }
+            if (q4) {
+                document.getElementById("javascript").removeAttribute("class");
+            }
+            if (q5) {
+                document.getElementById("python").removeAttribute("class");
+            }
         };
     };
 };
+
+
+
+
+// if (q1 && q2 && q3) {
+//     document.getElementById("python").removeAttribute("class");
+// } else if ((q1 && q2) || (q2 && q3) || (q1 && q3)) {
+//     document.getElementById("javaScript").removeAttribute("class");
+// } else if (q1 || q2 || q3) {
+//     document.getElementById("rust").removeAttribute("class");
+// } else {
+//     document.getElementById("errorMessage").removeAttribute("class");
+// };
